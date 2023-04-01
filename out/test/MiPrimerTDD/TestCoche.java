4aca4c4445a50ef49b1fdd2931f0dbbf85d2919b
jorge <jorselcer@alu.edu.gva.es>
2023-04-01 10:16:20 +0200

    Error en test

--- out/test/MiPrimerTDD/TestCoche.java
+++ out/test/MiPrimerTDD/TestCoche.java
@@ -3,8 +3,32 @@ import org.junit.Test;
 
 public class TestCoche {
     @Test
+    public void test_crear_coche(){
+        Coche nuevoCoche = new Coche();
+    }
+    @Test
     public void test_al_crear_un_coche_su_velocidad_es_cero(){
         Coche nuevoCoche = new Coche();
+        Assert.assertEquals(0,nuevoCoche.velocidad);
+    }
+    @Test
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.acelerar(30);
+        Assert.assertEquals(30,nuevoCoche.velocidad);
+    }
+    @Test
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad = 50;
+        nuevoCoche.decelerar(20);
+        Assert.assertEquals(30,nuevoCoche.velocidad);
+    }
+    @Test
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad = 50;
+        nuevoCoche.decelerar(80);
         Assert.assertEquals(0,nuevoCoche.velocidad);
     }
 }
