0c59ed22ee14ec10298ea987798cc89d45b3cca9
jorge <jorselcer@alu.edu.gva.es>
2023-04-01 08:40:36 +0200

    test velocidad cero

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -1,8 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCoche {
     @Test
-    public void test_crear_coche(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero(){
         Coche nuevoCoche = new Coche();
+        Assert.assertEquals(0,nuevoCoche.velocidad);
     }
 }
